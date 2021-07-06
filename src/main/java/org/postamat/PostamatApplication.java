1670fc7b8fd5fc4d21714f189ed97bc9fcbaf89e
Sarah <sarahwilldietoday@gmail.com>
2021-07-06 21:40:30 +0400

    Initial commit

---
+++ src/main/java/org/postamat/PostamatApplication.java
@@ -0,0 +1,13 @@
+package org.postamat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostamatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostamatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/postamat/PostamatApplicationTests.java
@@ -0,0 +1,13 @@
+package org.postamat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostamatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
